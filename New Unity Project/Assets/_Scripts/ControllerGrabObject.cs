447d975303a46494275447d0b2dc712919c9f9b6
Danielacoco <danielacoor6@gmail.com>
2017-03-02 17:15:28 -0800

    Adding controller methods

---
+++ New Unity Project/Assets/_Scripts/ControllerGrabObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+
+	private GameObject collidingObject;
+
+	private GameObject holdingObject;
+
+	private SteamVR_Controller.Device Controller{
+		get {return SteamVR_Controller.Input ((int)trackedObj.index); }
+	}
+
+	void Awake(){
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	public void OnTriggerEnter(Collider other){
+		SetCollidingObject(other);
+	}
+
+	public void OnTriggerStay(Collider other){
+		SetCollidingObject (other);
+	}
+	public void OnTriggerExit(Collider other){
+		if (!collidingObject) {
+			return;
+		}
+		collidingObject = null;
+	}
+
+	//Grabbing an object
+
+	private void
+
+
+
+}
